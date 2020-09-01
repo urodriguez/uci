3d6d0f8f7db188815e8704e58804289375524d82
uciel.rodriguez <uciel.rodriguez@endava.com>
2020-09-01 11:26:49 -0300

    master / WC: fix lint

--- WebClient/src/app/shared/base-http.service.ts
+++ WebClient/src/app/shared/base-http.service.ts
@@ -9,7 +9,7 @@ export class BaseHttpService {
   constructor(protected readonly httpClient: HttpClient,
               protected readonly appContext: AppContext) {
     if (window.location.host === 'localhost:4200') {
-      this.baseApiURL = 'http://www.ucirod.inventapp-dev.com:8080/WebApi/api/v1.0';  
+      this.baseApiURL = 'http://www.ucirod.inventapp-dev.com:8080/WebApi/api/v1.0';
     } else {
       this.baseApiURL = 'http://152.171.94.90:8080/WebApi/api/v1.0';
     }
